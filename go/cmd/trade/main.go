c18a6cceabfeefddbf1c08c45a6fdda1c8099ace
nalyx <joaorodriguez0106@gmail.com>
2023-06-27 20:11:51 -0300

    feat: app logic

---
+++ go/cmd/trade/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"sync"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/nalyx1/exchange-server/internal/infra/kafka"
+	"github.com/nalyx1/exchange-server/internal/market/dto"
+	"github.com/nalyx1/exchange-server/internal/market/entity"
+	"github.com/nalyx1/exchange-server/internal/market/transformer"
+)
+
+func main() {
+	ordersIn := make(chan *entity.Order)
+	ordersOut := make(chan *entity.Order)
+	wg := &sync.WaitGroup{}
+	defer wg.Wait()
+
+	kafkaMsgChan := make(chan *ckafka.Message)
+	configMap := &ckafka.ConfigMap{
+		"bootstrap.servers": "host.docker.internal:9094",
+		"group.id":          "exchange-server",
+		"auto.offset.reset": "latest",
+	}
+	producer := kafka.NewProducer(configMap)
+	consumer := kafka.NewConsumer(configMap, []string{"input"})
+
+	go consumer.Consume(kafkaMsgChan)
+
+	book := entity.NewBook(ordersIn, ordersOut, wg)
+	go book.Trade()
+
+	go func() {
+		for msg := range kafkaMsgChan {
+			wg.Add(1)
+			fmt.Println(string(msg.Value))
+			tradeInput := dto.TradeInput{}
+			err := json.Unmarshal(msg.Value, &tradeInput)
+			if err != nil {
+				panic(err)
+			}
+			order := transformer.TransformInput(tradeInput)
+			ordersIn <- order
+		}
+	}()
+
+	for res := range ordersOut {
+		output := transformer.TransformOutput(res)
+		outputJson, err := json.MarshalIndent(output, "", " ")
+		fmt.Println(string(outputJson))
+		if err != nil { 
+			fmt.Println(err)
+		}
+		producer.Publish(outputJson, []byte("output"), "output")
+	}
+}
